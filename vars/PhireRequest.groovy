ffe51bcbfe4e36bffa1bccb7041a095b09a325cb
Nevin <16478740+neonevin@users.noreply.github.com>
2025-06-01 18:10:54 +1000

    debug

--- vars/PhireRequest.groovy
+++ vars/PhireRequest.groovy
@@ -47,6 +47,7 @@ String call(String userid, String password, String phireTktId, String ticketPrjI
         phiDomainId=""
         phireId=""
     }
+     println 'phiDomainId '+ phiDomainId + 'phiCrNum ' + phiCrNum +'### end.'
     
     //                          (phiDomainId, phiCrNum, tktStatus, phiAssignee, assigneeEmail, phiMigrType, ticketId, rqstUsr, rqstEmail) 
    //        (phiDomainId, phiCrNum, ticketPrjId, ticketAppId, ticketType, tktStatus, tktAssignee, assigneeEmail, phiMigrType, ticketId, phiTitle, rqstUsr, rqstEmail) {
