c7505c39a4e3fbbe9440a678ce88168ba0ee4960
neonevin <nevinprasannan@gmail.com>
2026-02-10 19:59:25 +1000

    debug002

--- vars/PhireCallBackReq.groovy
+++ vars/PhireCallBackReq.groovy
@@ -1,6 +1,7 @@
 import org.phire.*
 
 def call(String strReqMsg) {
+    println "in PhireCallBackReq.groovy - call function"
     def phireCallbackActReq = new PhireCallbackActReq(strReqMsg)
     return phireCallbackActReq
 }
