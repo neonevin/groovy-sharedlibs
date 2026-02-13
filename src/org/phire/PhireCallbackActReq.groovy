15c6aef92ac8c8f7b4a76179b71022be784fe22b
neonevin <nevinprasannan@gmail.com>
2026-02-13 17:45:31 +1000

    debug 09

--- src/org/phire/PhireCallbackActReq.groovy
+++ src/org/phire/PhireCallbackActReq.groovy
@@ -32,11 +32,12 @@ class PhireCallbackActReq {
     def PhireCallbackActReq(strReqMsg)
     {
         def jsonSlurper = new JsonSlurper()
-        println strReqMsg
+        println "req constr" + strReqMsg
         def reqObj =  jsonSlurper.parseText(strReqMsg)
         assert reqObj instanceof Map
-        println reqObj.toString()
-        this.PhireCallbackActReq(reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_DOMAIN_ID, reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CR_NUM, reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_STATUS, reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CALLBA_REQ.DTTL_TICKET_ID, reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_MIGR_TYPE, reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_ACTION_TYPE, reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_ACTION_DESCR)
+        println "req cnstr 2 " + reqObj.toString()
+        reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_ID
+        this.PhireCallbackActReq(reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_DOMAIN_ID, reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CR_NUM, reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_STATUS, reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_ID, reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_MIGR_TYPE, reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_ACTION_TYPE, reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_ACTION_DESCR)
 
     }
 }
