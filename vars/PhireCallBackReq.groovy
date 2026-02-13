ca1c33a13767a6ca8f989badcc7d0dcfe5615e5a
neonevin <nevinprasannan@gmail.com>
2026-02-13 17:41:25 +1000

    debug 08

--- vars/PhireCallBackReq.groovy
+++ vars/PhireCallBackReq.groovy
@@ -13,10 +13,10 @@ def call(String strReqMsg) {
             println "req obj - domain id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_DOMAIN_ID
             println "req obj - cr num" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CR_NUM
             println "req obj - ticket status" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_STATUS
-            println "req obj - ticket id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CALLBA_REQ.DTTL_TICKET_ID
+            println "req obj - ticket id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_ID
             println "req obj - migr type" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_MIGR_TYPE
             println "req obj - action type" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_ACTION_TYPE
-            println "req obj - action descr" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_ACTION_DESCR         
+            println "req obj - action descr" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_ACTION_DESCR
     def phireCallbackActReq = new PhireCallbackActReq(strReqMsg)
      println "in PhireCallBackReq.groovy - B4 return" + phireCallbackActReq.toString()
     return phireCallbackActReq
