a2535986a5da99c0b57cc50fd024a4f6ca53a925
neonevin <nevinprasannan@gmail.com>
2026-02-13 17:30:57 +1000

    debug 06

--- vars/PhireCallBackReq.groovy
+++ vars/PhireCallBackReq.groovy
@@ -10,6 +10,13 @@ def call(String strReqMsg) {
             def reqObj =  jsonSlurper.parseText(strReqMsg)
             assert reqObj instanceof Map
             println "req obj" +reqObj.toString()
+            printlf "req obj - domain id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_DOMAIN_ID
+            printlf "req obj - cr num" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CR_NUM
+            printlf "req obj - ticket status" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_STATUS
+            printlf "req obj - ticket id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CALLBA_REQ.DTTL_TICKET_ID
+            printlf "req obj - migr type" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_MIGR_TYPE
+            printlf "req obj - action type" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_ACTION_TYPE
+            printlf "req obj - action descr" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_ACTION_DESCR         
     def phireCallbackActReq = new PhireCallbackActReq(strReqMsg)
      println "in PhireCallBackReq.groovy - B4 return" + phireCallbackActReq.toString()
     return phireCallbackActReq
