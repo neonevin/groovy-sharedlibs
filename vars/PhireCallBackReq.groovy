8bcdb8a710635f7207b4bd5ba1a29a6b4400d5a5
neonevin <nevinprasannan@gmail.com>
2026-02-13 17:11:55 +1000

    debug 01

--- vars/PhireCallBackReq.groovy
+++ vars/PhireCallBackReq.groovy
@@ -3,6 +3,9 @@ import org.phire.*
 @NonCPS
 def call(String strReqMsg) {
     println "in PhireCallBackReq.groovy - call function" + strReqMsg
+            def reqObj =  jsonSlurper.parseText(strReqMsg)
+        assert reqObj instanceof Map
+        println "req obj" +reqObj.toString()
     def phireCallbackActReq = new PhireCallbackActReq(strReqMsg)
      println "in PhireCallBackReq.groovy - B4 return" + phireCallbackActReq.toString()
     return phireCallbackActReq
