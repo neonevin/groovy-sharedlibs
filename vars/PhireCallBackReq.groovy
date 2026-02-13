4597c93e4c09854a37e17a2e6191d4f4420093ee
neonevin <nevinprasannan@gmail.com>
2026-02-13 17:39:15 +1000

    debug 07

--- vars/PhireCallBackReq.groovy
+++ vars/PhireCallBackReq.groovy
@@ -9,14 +9,14 @@ def call(String strReqMsg) {
             def jsonSlurper = new JsonSlurper()
             def reqObj =  jsonSlurper.parseText(strReqMsg)
             assert reqObj instanceof Map
-            println "req obj" +reqObj.toString()
-            printlf "req obj - domain id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_DOMAIN_ID
-            printlf "req obj - cr num" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CR_NUM
-            printlf "req obj - ticket status" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_STATUS
-            printlf "req obj - ticket id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CALLBA_REQ.DTTL_TICKET_ID
-            printlf "req obj - migr type" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_MIGR_TYPE
-            printlf "req obj - action type" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_ACTION_TYPE
-            printlf "req obj - action descr" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_ACTION_DESCR         
+            println "req obj in call" +reqObj.toString()
+            println "req obj - domain id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_DOMAIN_ID
+            println "req obj - cr num" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CR_NUM
+            println "req obj - ticket status" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_TICKET_STATUS
+            println "req obj - ticket id" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_CALLBA_REQ.DTTL_TICKET_ID
+            println "req obj - migr type" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_MIGR_TYPE
+            println "req obj - action type" + reqObj.DTTL_PHI_MIGR_STAT_REQ.DTTL_ACTION_TYPE
+            println "req obj - action descr" + reqObj.DTTL_PHI_MIGR_STAT_REQ.PHI_ACTION_DESCR         
     def phireCallbackActReq = new PhireCallbackActReq(strReqMsg)
      println "in PhireCallBackReq.groovy - B4 return" + phireCallbackActReq.toString()
     return phireCallbackActReq
